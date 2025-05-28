c1e6b80160fe7a8dcc8e76e4560a4459001418ca
miguel2433 <miguelitocap12@gmail.com>
2025-05-28 18:07:13 -0300

    boundaries

--- index.js
+++ index.js
@@ -1,14 +1,48 @@
 const canvas = document.querySelector('canvas')
 const c = canvas.getContext('2d')
 
-console.log(collisions)
-
 canvas.width = 1024
 canvas.height = 576
 
-for( let i = 0; i < collisions.length; i ++){
-  
+const collisionsMap = []
+
+for( let i = 0; i < collisions.length; i += 70){
+  collisionsMap.push(collisions.slice(i, 70 + i))
 }
+
+class Boundary{
+  static width = 48
+  static height = 48
+  constructor({position}){
+    this.position = position
+    this.width = 48
+    this.height = 48
+  }
+  draw(){
+    c.fillStyle = 'red'
+    c.fillRect(this.position.x, this.position.y, this.width, this.height)
+  }
+}
+
+const boundaries = []
+
+const offset ={
+  x : -737,
+  y : -650
+}
+collisionsMap.forEach((row, i)=> {
+  row.forEach((symbol, j) =>{
+    if(symbol === 1025)
+    boundaries.push(new Boundary({
+      position:{
+        x: j * Boundary.width + offset.x,
+        y: i * Boundary.height+ offset.y
+      }
+    }))
+  })
+})
+
+console.log(boundaries)
 
 const image = new Image()
 image.src = './Imagenes/Town.png'
@@ -17,7 +51,7 @@ const playerImage = new Image()
 playerImage.src = './Imagenes/playerDown.png'
 
 class Sprite {
-  constructor({position, image}){
+  constructor({position, velocity,image}){
     this.position = position
     this.image = image
   }
@@ -27,10 +61,11 @@ class Sprite {
   }
 }
 
+
 const background = new Sprite({
   position:{
-    x: -737,
-    y: -550
+    x: offset.x,
+    y: offset.y
   },
   image: image
 })
@@ -45,6 +80,10 @@ const keys = {
 function animate(){
   window.requestAnimationFrame(animate)
   background.draw()
+
+  boundaries.forEach(boundary =>{
+    boundary.draw()
+  })
 
   c.drawImage(
     playerImage,
