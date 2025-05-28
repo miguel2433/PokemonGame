2e2f816c673b0399a09aba115c13cf74a028b9a1
miguel2433 <miguelitocap12@gmail.com>
2025-05-28 18:28:00 -0300

    collision

--- index.js
+++ index.js
@@ -51,16 +51,45 @@ const playerImage = new Image()
 playerImage.src = './Imagenes/playerDown.png'
 
 class Sprite {
-  constructor({position, velocity,image}){
+  constructor({position, velocity,image, frames = {max: 1}}){
     this.position = position
     this.image = image
+    this.frames = frames
+
+    this.image.onload = () =>{
+      this.width = this.image.width / this.frames.max
+      this.height = this.image.height 
+      console.log(this.width)
+      console.log(this.height)
+    }
   }
 
   draw(){
-    c.drawImage(this.image, this.position.x, this.position.y)
+    c.drawImage(
+      this.image,
+      0, 0,
+      this.image.width / this.frames.max,
+      this.image.height,
+      this.position.x,
+      this.position.y,
+      this.image.width / this.frames.max,
+      this.image.height
+    )
   }
 }
+/*canvas.width / 2 - this.image.width / 4 / 2,
+canvas.height / 2 - this.image.height / 2,*/
 
+const player = new Sprite({
+  position:{
+    x: canvas.width / 2 - 192 / 4 / 2,
+    y: canvas.height / 2 - 68 / 2
+  },
+  image: playerImage,
+  frames:{
+    max: 4
+  }
+})
 
 const background = new Sprite({
   position:{
@@ -77,30 +106,46 @@ const keys = {
   d: { pressed: false }
 }
 
+const testBoundary = new Boundary({
+  position:{
+    x: 400,
+    y: 400
+  }
+})
+
+const movables = [background,testBoundary]
+
 function animate(){
   window.requestAnimationFrame(animate)
   background.draw()
 
-  boundaries.forEach(boundary =>{
+  /*boundaries.forEach(boundary =>{
     boundary.draw()
-  })
+  })*/
+  testBoundary.draw()
+  player.draw()
 
-  c.drawImage(
-    playerImage,
-    0, 0,
-    playerImage.width / 4,
-    playerImage.height,
-    canvas.width / 2 - playerImage.width / 4 / 2,
-    canvas.height / 2 - playerImage.height / 2,
-    playerImage.width / 4,
-    playerImage.height
-  )
+  if(player.position.x + player.width >= testBoundary.position.x){
+    console.log("colliding")
+  }
 
   // Movimiento (permitiendo múltiples teclas)
-  if (keys.w.pressed && lastkey === 'w') background.position.y += 3
-  if (keys.s.pressed && lastkey === 's') background.position.y -= 3
-  if (keys.a.pressed && lastkey === 'a') background.position.x += 3
-  if (keys.d.pressed && lastkey === 'd') background.position.x -= 3
+  if (keys.w.pressed && lastkey === 'w') {
+    movables.forEach((movable) => {
+      movable.position.y += 3
+    })}
+  if (keys.s.pressed && lastkey === 's') {
+    movables.forEach((movable) => {
+      movable.position.y -= 3
+    })}
+  if (keys.a.pressed && lastkey === 'a'){
+    movables.forEach((movable) => {
+      movable.position.x += 3
+    })}
+  if (keys.d.pressed && lastkey === 'd') {
+    movables.forEach((movable) => {
+      movable.position.x -= 3
+    })}
 }
 
 let imagesLoaded = 0
